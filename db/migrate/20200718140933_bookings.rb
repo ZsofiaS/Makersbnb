db3169069bf5622125c319ff1bfd69c879978c37
GarethWoodman <gareth.woodman92@gmail.com>
2020-07-18 15:25:48 +0100

    Added bookings ActiveRecord migration

--- db/migrate/20200718140933_bookings.rb
+++ db/migrate/20200718140933_bookings.rb
@@ -1,4 +1,11 @@
 class Bookings < ActiveRecord::Migration[6.0]
   def change
+    create_table :bookings do |t|
+      #id SERIAL PRIMARY KEY, date DATE, space_id INTEGER, user_id INTEGER, status VARCHAR(20));
+      t.date :date
+      t.integer :space_id
+      t.integer :user_id
+      t.string :status
+    end
   end
 end
